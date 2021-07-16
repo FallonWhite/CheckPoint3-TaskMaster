d130fb3ca33de1dc919500a9c5a27e52c280c710
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-16 16:21:31 -0600

    save after lab

--- app/AppState.js
+++ app/AppState.js
@@ -1,21 +1,23 @@
-import Value from "./Models/Value.js"
+import List from "./Models/List.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
 class AppState extends EventEmitter {
-  /** @type {Value[]} */
-  values = []
-}
-
+  /** @type {List[]} */
+  lists = [
+    new List({
+      name: 'Friday',
+    }),
+  
 export const ProxyState = new Proxy(new AppState(), {
-  get(target, prop) {
-    isValidProp(target, prop)
-    return target[prop]
-  },
-  set(target, prop, value) {
-    isValidProp(target, prop)
-    target[prop] = value
-    target.emit(prop, value)
-    return true
-  }
-})
+      get(target, prop) {
+        isValidProp(target, prop)
+        return target[prop]
+      },
+      set(target, prop, value) {
+        isValidProp(target, prop)
+        target[prop] = value
+        target.emit(prop, value)
+        return true
+      }
+    })
--- app/Controllers/ListsController.js
+++ app/Controllers/ListsController.js
@@ -2,6 +2,30 @@ import { ProxyState } from "../AppState.js";
 import { listsService } from "../Services/ListsService.js";
 
 
+function _draw() {
+  let template = ''
+  ProxyState.lists.forEach(list => {
+    template += list.Template
+  })
+  document.getElementById('lists').innerHTML = template
+}
+
+export default class ListsController {
+  constructor() {
+    ProxyState.on('lists', _draw)
+    ProxyState.on('lists', () => { console.log('new list') })
+    _draw()
+  }
+  createList() {
+    event.preventDefault()
+    let form = event.target
+    let rawList = {
+      name: form.name.value
+    }
+    listsService.createList(rawList)
+    form.reset()
+  }
+}
 
 
 
