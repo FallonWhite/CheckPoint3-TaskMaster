0e2018933791c0d80afdde540c96f94354102d59
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-16 20:14:22 -0600

    save

--- app/AppState.js
+++ app/AppState.js
@@ -4,11 +4,11 @@ import { isValidProp } from "./Utils/isValidProp.js"
 
 class AppState extends EventEmitter {
   /** @type {List[]} */
+
   lists = [
     new List({
       name: 'Friday',
-    }),
-  
+    })
 export const ProxyState = new Proxy(new AppState(), {
       get(target, prop) {
         isValidProp(target, prop)
