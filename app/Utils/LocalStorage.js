3d82bd41c54f71e8a36ac4745038a77a65b0515c
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-17 12:44:06 -0600

    getter loadstate

--- app/Utils/LocalStorage.js
+++ app/Utils/LocalStorage.js
@@ -3,14 +3,14 @@ import List from "../Models/List.js";
 
 
 export function saveState() {
-  localStorage.setItem('MyList', JSON.stringify({
+  localStorage.setItem('Friday', JSON.stringify({
     lists: ProxyState.lists,
   }))
   console.log('saved state', ProxyState)
 }
 
 export function loadState() {
-  let data = JSON.parse(localStorage.getItem('MyList'))
+  let data = JSON.parse(localStorage.getItem('Friday'))
   console.log(data)
   if (data != null) {
     ProxyState.lists = data.lists.map(list => new List(l))
