cac2e1fa2520548230024f560a86f33b4bd6f3de
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-17 15:12:51 -0600

    debug save

--- app/Models/List.js
+++ app/Models/List.js
@@ -7,7 +7,7 @@ export default class List {
   get Template() {
     return `
     <div class="col-md-3 col-sm-2 my-3">
-      <div class="list shadow" style="background-color: ${this.color}>
+      <div class="list shadow" style="background-color: ${this.color}">
           <div class="p-3">
               <div class="text-center">
                   <p><b>${this.name}</b></p >
